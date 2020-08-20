01622c35fee21fbb808f5a974b64985be514225c
Kelvin Fichter <kelvinfichter@gmail.com>
2020-08-20 14:54:20 -0400

    Removed build directory

--- dist/ovm/utils/contracts/index.d.ts
+++
@@ -1,4 +0,0 @@
-import VM from '../../../index';
-import { OVMContract } from '../ovm-contract';
-export declare const OVM_CONTRACT_DEFS: any;
-export declare const makeOvmContract: (vm: VM, name: string) => OVMContract;
--- dist/ovm/utils/ovm-transaction.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import VM from '../../index';
-import { RunTxResult } from '../../runTx';
-export declare const sendOvmTransaction: (vm: VM, calldata: string, from: string | Buffer, to: string | Buffer | undefined) => Promise<RunTxResult>;
